1ef5aaced68c6b01b386f3a6080b42ad0cdea1b3
darrinjamessneddon <140578555+darrinjamessneddon@users.noreply.github.com>
2026-08-05 21:36:36 +0930

    Add error module for error-handling
    Added an error module for handling errors in other modules.

--- src/lib.rs
+++ src/lib.rs
@@ -51,6 +51,9 @@
 use num_bigint::BigUint;
 use num_traits::{One, FromPrimitive, ToPrimitive};
 
+pub mod error {
+    /// Add a module for error-handling in the other modules
+}
 pub mod spouge_reciprocal {
     use f256::f256;
 
