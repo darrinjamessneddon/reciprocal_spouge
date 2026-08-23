78a863a65b7c6840f5d3f7b8c0cacc839350dd93
darrinjamessneddon <140578555+darrinjamessneddon@users.noreply.github.com>
2026-08-23 13:08:27 +0930

    Add comments about ln_gamma function continuity
    Added comments regarding the continuity of the ln_gamma function for complex numbers.

--- src/core/lngamma/lngamma.rs
+++ src/core/lngamma/lngamma.rs
@@ -1,4 +1,7 @@
 pub mod lngamma {
+    // The ln_gamma function for complex z is not continuous. It is important to consider zeros, singularities and discontinuities
+    // of the function before creating it.
+    
     pub fn ln_gamma() {
         // add code here
     }
