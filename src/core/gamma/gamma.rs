730dabd57bd37b3598d785c3e90e27c7cb11b8a1
darrinjamessneddon <140578555+darrinjamessneddon@users.noreply.github.com>
2026-09-10 16:59:27 +0930

    Update gamma.rs

--- src/core/gamma/gamma.rs
+++ src/core/gamma/gamma.rs
@@ -46,9 +46,8 @@ pub mod gamma {
             // Use the reflection formula for the gamma function
             // gamma(z) = pi / (sin(pi * z) * gamma(1 - z))
             let one = Complex256::new(Float256::from(1.0), Float256::from(0.0));
-            let z_minus_one = z.sub(one);
             let one_minus_z =
-                Complex256::new(Float256::from(1.0) - z_minus_one.re, -z_minus_one.im);
+                Complex256::new(Float256::from(1.0) - z.re, -z.im);
             let pi = PI;
             let pi_complex = Complex256::new(pi, Float256::from(0.0));
             let sin_pi_z = (pi_complex.mul(z)).sin();
