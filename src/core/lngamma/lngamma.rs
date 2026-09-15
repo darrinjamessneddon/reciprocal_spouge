89899968649d4df08dff2d094a11e4ff6ee9bac2
darrinjamessneddon <140578555+darrinjamessneddon@users.noreply.github.com>
2026-09-15 12:49:57 +0930

    Update lngamma.rs
    Update comments/ recommendations for how to create the code

--- src/core/lngamma/lngamma.rs
+++ src/core/lngamma/lngamma.rs
@@ -2,6 +2,11 @@ pub mod lngamma {
     // The ln_gamma function for complex z is not continuous. It is important to consider zeros, singularities and discontinuities
     // of the function before creating it.
 
+    // ln_gamma(z + 1) = (z + 0.5)* ln(z + a) - (z + a) + ln {co + (sum from k = 1 to a - 1 (ck/(z - 1 + k))
+
+    // If the real part of z is small Re(z) < 0.5, we need to use the reflection formula:
+    // ln(z) = ln(pi) - lnsin(pi * z) - ln_gamma(1 - z) before applying the approximation.
+
     pub fn ln_gamma() {
         // add code here
     }
